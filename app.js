246e98969a3ac4151f52af98c4f6dc7d324c9b6a
sucsnj <78510176+sucsnj@users.noreply.github.com>
2025-12-11 11:43:27 +0000

    gravando e pegando o formulário de um json

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const formRouter = require('./routes/form');
+const leituraRouter = require('./routes/form');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', formRouter);
+app.use('/', leituraRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
