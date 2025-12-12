21f0844960341ca367f7ec9aba4e374820346432
sucsnj <78510176+sucsnj@users.noreply.github.com>
2025-12-12 15:23:37 +0000

    get para mostrar o curriculo pronto

--- routes/form.js
+++ routes/form.js
@@ -23,9 +23,22 @@ router.get('/leitura', (req, res) => {
   if (fs.existsSync("dados.json")) {
     const registros = JSON.parse(fs.readFileSync("dados.json"));
     res.json(registros); // retorna todos os registros
+
   } else {
     res.json({ mensagem: "Nenhum dado encontrado" });
   }
+});
+
+router.get('/form', (req, res) => {
+  let registros = [];
+
+  if (fs.existsSync("dados.json")) {
+    registros = JSON.parse(fs.readFileSync("dados.json"));
+  }
+
+  const ultimoRegistro = registros[registros.length - 1];
+
+  res.render('form', { dados: ultimoRegistro }); 
 });
 
 module.exports = router;
